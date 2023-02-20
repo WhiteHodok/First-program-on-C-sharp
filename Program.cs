c13ace6a4f47ef46ff14c62f7b3136d2894775a5
WhiteHodok <balanaraus@gmail.com>
2023-02-20 19:56:20 +0600

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите свое имя: ");
+var name = Console.ReadLine();       // вводим имя
+Console.WriteLine($"Привет {name}");    // выводим имя на консоль
